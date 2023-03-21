f8f55fdf27702c3ae7572371e3221985226b65fa
Zsidó Ruben <zsido.ruben@jedlik.helyi>
2023-03-21 11:10:39 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const cardImages = [
@@ -10,10 +11,26 @@ const cardImages = [
 ];
 
 function App() {
+const [cards, setCards] = useState([]);
+const [turns, setTurns] = useState(0);
+
+  //shuffle
+
+const shuffleCards = ()=>{
+  const shuffledCards = [...cardImages, ...cardImages]
+  .sort(()=> Math.random() - 0.5)
+  .map((card)=> ({...card, id: Math.random()}))
+  
+   setCards(shuffledCards)
+  setTurns(0)
+}
+
+console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards} >New Game</button>
     </div>
   );
 }
